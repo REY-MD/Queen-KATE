6872a5636169d6024f0554c1af1c124dd89b29ba
REY-MD <humphreymbise01@gmail.com>
2026-04-12 16:51:30 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of ZEZE47-MD bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_uptime of QUEEN-KATE bot is: ${runtime(process.uptime())}_*`) 
 
    
 
